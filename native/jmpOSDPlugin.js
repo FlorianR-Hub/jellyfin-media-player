8ecedca02dc7eac5f9d6df5f8ee057dc2b960559
Florian R. <florian.rifflart@gmail.com>
2026-02-20 18:08:21 +0100

    feat: reduce click delay for faster play/pause response

--- native/jmpOSDPlugin.js
+++ native/jmpOSDPlugin.js
@@ -16,13 +16,16 @@ class jmpOSDPlugin {
         window.setTimeout = function(callback, delay, ...args) {
             // If the delay is 3000ms (OSD auto-hide), change it to 1000ms
             if (delay === 3000) {
-                console.log('JMP OSD Plugin: Changing OSD timeout from 3000ms to 1000ms');
                 return originalSetTimeout(callback, 1000, ...args);
             }
+
+            // If the delay is 300ms (common click delay), change it to 50ms
+            if (delay === 300) {
+                return originalSetTimeout(callback, 50, ...args);
+            }
+            
             return originalSetTimeout(callback, delay, ...args);
         };
-
-        console.log('JMP OSD Plugin: OSD timeout override initialized');
     }
 }
 
